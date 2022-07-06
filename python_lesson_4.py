046f882c165aed4d2c498ee1591eb2167b43eb95
Calvin-alis <alis.demiurg@gmail.com>
2022-07-06 19:13:15 +0300

    Start lesson_3

---
+++ python_lesson_4.py
@@ -0,0 +1,17 @@
+# Lesson 3 end and Lesson 4
+
+#def test(a):
+#    return True if a >= 18 and a <= 27  else False
+arr = [1,3,5,7,90, -1]
+print(sum(arr), max(arr), min(arr))
+counter = 0
+max_of = 0
+min_of = 0
+#for i in arr:
+#    if i > max_of:
+#        max_of = i
+#    elif i < min_of:
+#        min_of = i
+#    counter += i
+#print(counter, max_of, min_of)
+
