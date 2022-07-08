01eb9a5decd7c374152cec51aea18d123f08d44c
Calvin-alis <alis.demiurg@gmail.com>
2022-07-08 19:48:21 +0300

    Work with func

--- python_lesson_5.py
+++ python_lesson_5.py
@@ -13,15 +13,44 @@ def div(number):
     return True if number % 2 == 0 else False
 
 
-for i in range(1, 100):
-    if len(res) == 15:
-        break
-    else:
-        if div(i) == True:
-            res.append(i)
+#for i in range(1, 100):
+#    if len(res) == 15:
+#        break
+#    else:
+#       if div(i) == True:
+#       res.append(i)
 
-print(res)
+# print(res)
 
+#while True:
+   # input_value = input('enter quit if u want stop: ')
+  #  if input_value == 'quit':
+   #     break
+   # else:
+   #     print(input_value)
+
+
+#input_value = input('enter quit if u want stop: ')
+#while input_value != 'quit':
+#    input_value = input('enter quit if u want stop: ')
+ #   print(input_value)
+
+def check_div_and_cont(enter_range:int) -> None:
+    for i in range(int(enter_range)):
+        if i % 2 == 0:
+            continue
+        else:
+            print(i)
+
+#a = check_div_and_cont(100)
+#print(type(a))
+# print(check_div_and_cont(100.5))
+
+#for i in range(0, 100):
+#    if i % 2 == 1:
+#        pass
+#    else:
+#        print(i)
 #end_prog = dt.datetime.now()
 
 
