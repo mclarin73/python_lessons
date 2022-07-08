b40d13996068a82f6cdce157b373469014b93181
Calvin-alis <alis.demiurg@gmail.com>
2022-07-08 22:08:34 +0300

    create simple pet

--- python_lesson_5.py
+++ python_lesson_5.py
@@ -92,28 +92,51 @@ print(obj['name'], obj['books'])
 for i in obj['books']:
     print(i)
 
-a_input = input('Enter value:')
-b_input = input('Enter second value: ')
+#a_input = input('Enter value:')
+#b_input = input('Enter second value: ')
 
-try:
-    a_input = int(a_input)
-    b_input = int(b_input)
-    print(a_input / b_input)
-except:
+#try:
+#    a_input = int(a_input)
+#    b_input = int(b_input)
+#    print(a_input / b_input)
+#except:
     #print('No Value')
-    print('Try Again!')
-else:
-    print(a_input)
+#    print('Try Again!')
+#else:
+#    print(a_input)
 
 
 
 
 # Create simple weather-app
+
+
+
+def check_input_city(name:str) -> str:
+    # First Variant
+    return ''.join(i for i in name if i.isalpha() or i in [' ', '-']).capitalize()
+    # Second Variant
+    # res = ''
+    #for i in name:
+    #    if i.isalpha() or i in [' ', '-']:
+            # res = res + i
+            # .... save in result
+
+
+def get_weather(name:str, api:str):
+    try:
+        url_api = f'https://api.openweathermap.org/data/2.5/weather?q={name_clear}&appid={api_name}&units=metric'
+        response = requests.get(url_api)
+        data_json = response.json()
+        return data_json['main']['temp']
+    except:
+        print('Wrong name api, or wrong name city')
+
 api_name = '33d99a1c99c5ea82e6aaff8592cd6fc3'
-name_of_city = 'Lviv'
+des_input = input('Enter something to continue else enter stop: ')
+while des_input != 'stop':
 
-
-
-url_api = f'https://api.openweathermap.org/data/2.5/weather?q={name_of_city}&appid={api_name}&units=metric'
-response = requests.get(url_api)
-data_json = response.json()
+    name_of_city = input('Input City:')
+    name_clear = check_input_city(name_of_city)
+    print(f'Weather in city {name_clear} is {get_weather(name_clear, api= api_name)}')
+    des_input = input('Enter something to continue else enter stop: ')
