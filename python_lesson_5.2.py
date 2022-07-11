98c602cd6f95b65e9e567d337b348a242f7ad575
Calvin-alis <alis.demiurg@gmail.com>
2022-07-11 19:52:11 +0300

    Map, list,reduct

--- python_lesson_5.2.py
+++ python_lesson_5.2.py
@@ -1,10 +1,44 @@
 # filter map reduce
+from functools import  reduce
+
+
+def plus_two_value(a, b):
+    return a + b
+
 
 def div_value(item):
     return True if item % 2 == 0 else False
 
-a = list(range(100))
+
+def change_minus(item):
+    return item * -1
+
+
+def check_string(item):
+    return True if item in ['a', 'b', 'c'] else False
+
+
+string = 'ardbc'
+print(list(filter(check_string, string)))
+
+a = list(range(10))
 b = list(filter(div_value, a))
 c = list(filter(lambda x: x % 2 == 1, a))
-print(b)
-print(c)
+#print(b)
+#print(c)
+
+a_map = list(range(-10, 10))
+print(a_map)
+b_map = list(map(change_minus, a_map))
+c_map = list(map(lambda x: x ** 2, a_map))
+print(b_map)
+print(c_map)
+
+a_reduce = [14,5,7,8,3,4,12, 68, -10]
+string_reduce = ['s', 'a', 's', 'c', 'h', 'a']
+
+b_reduct = reduce(lambda x, y: x + y, a_reduce)
+c_reduce = reduce(lambda x, y: x + y, string_reduce)
+d_reduct = reduce(plus_two_value,string_reduce )
+print(c_reduce)
+print(d_reduct)
