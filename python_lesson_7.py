3078c200c1030e1c8fa36304affa0175b4fdc7cb
Calvin-alis <alis.demiurg@gmail.com>
2022-07-13 20:37:59 +0300

    first_part

--- python_lesson_7.py
+++ python_lesson_7.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 
 a = np.array([-1,1,2,3,4,5,6,7], dtype=float)
@@ -7,7 +8,7 @@ print(f'Max of a: {a.max()}\nMin of a:{a.min()}')
 print(a.max(), a.min(), a.mean(), a.sum(), a.std())
 
 x_2d = np.array([
-    [1,2,3],
+    [12,24,36],
     [4,5,6]
 ])
 print(x_2d[0][2], x_2d[0].min(), x_2d[1].min())
@@ -17,3 +18,10 @@ x = np.array([[[i + j + k for k in range(5)] for j in range(4)] for i in range(3
 print(x_2d.shape)
 print(x.shape)
 print(x.size, x_2d.size)
+
+one_matrix = np.ones((4, 1), dtype=np.float)
+print(one_matrix, one_matrix.size, one_matrix.shape)
+print(x_2d[0].sum(), x_2d[1].sum(), len(x_2d[0]), x_2d[0].size)
+print('Second part of lesson')
+print(x_2d)
+print(x_2d[1::])
